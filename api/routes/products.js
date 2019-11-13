1ec3a3fb52f3e3a3d6031755d283434912c86db7
hkwarui <info.warui@gmail.com>
2019-11-13 22:37:54 +0300

    First Commit

---
+++ api/routes/products.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Handling GET request to / Products"
+  });
+});
+router.post("/", (req, res, next) => {
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+  }
+    res.status(201).json({
+        message: "Handling POST request to / Products",
+        createdProduct :product
+  });
+});
+
+router.get("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  if (id === "special") {
+    res.status(200).json({
+      message: "You discovered the special ID"
+    });
+  } else {
+    res.status(200).json({
+      message: "You passed ID"
+    });
+  }
+});
+
+router.patch("/:productId", (req, res, next) => {
+  res.status(200).json({
+    message: "Updated product"
+  });
+});
+
+router.delete("/:productId", (req, res, next) => {
+  res.status(200).json({
+    message: "Delete  product!"
+  });
+});
+
+module.exports = router;
